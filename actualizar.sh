b6653d143dd8127fd8f4a43a4777b2e6b5246aea
matiassy <78431535+matiassy@users.noreply.github.com>
2024-10-17 23:23:33 -0300

    Update actualizar.sh

--- actualizar.sh
+++ actualizar.sh
@@ -4,24 +4,18 @@
 archivo_salida="sistema_info.txt"
 
 # Obtener información del archivo /etc/*release
-echo "Información de /etc/*release:" > "$archivo_salida"
+echo "Sistema operativo:" > "$archivo_salida"
 cat /etc/*release >> "$archivo_salida"
-echo -e "\n----------------------------------------" >> "$archivo_salida"
-
-# Actualizar la lista de paquetes
-echo "Actualizando la lista de paquetes..." >> "$archivo_salida"
-apt update >> "$archivo_salida" 2>&1
-echo -e "\n----------------------------------------" >> "$archivo_salida"
 
 # Listar paquetes actualizables
-echo "Paquetes actualizables:" >> "$archivo_salida"
+echo "Lista de actualizaciones:" >> "$archivo_salida"
 apt list --upgradable >> "$archivo_salida"
-echo -e "\n----------------------------------------" >> "$archivo_salida"
 
-# Obtener la versión de PVE
-echo "Versión de PVE (Proxmox):" >> "$archivo_salida"
-pveversion >> "$archivo_salida" 2>&1
-echo -e "\n----------------------------------------" >> "$archivo_salida"
+# Detectar si el sistema es Proxmox y obtener la versión de PVE
+if command -v pveversion >/dev/null 2>&1; then
+    echo "Versión de PVE (Proxmox):" >> "$archivo_salida"
+    pveversion >> "$archivo_salida" 2>&1
+fi
 
 # Mensaje de finalización
 echo "La información se ha guardado en $archivo_salida"
